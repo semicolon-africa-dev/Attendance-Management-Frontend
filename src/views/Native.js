a253cede0b20efb5e76a9233c989367db2b40e60
kindyluv <onyeukwuamara@gmail.com>
2021-12-09 22:11:36 +0100

    Native view added

---
+++ src/views/Native.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import AdminSideBar from "../components/AdminSideBar";
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <AdminSideBar/>
+            </div>
+        );
+    }
+}
+
+export default Admin;

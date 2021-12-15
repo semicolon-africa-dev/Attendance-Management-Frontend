fe7a284869f28fd0b5413f4b5082dbb9dfaf267d
kindyluv <onyeukwuamara@gmail.com>
2021-12-15 03:04:33 +0100

    ..

---
+++ src/views/admin/Admin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react";
+import AdminSideBar from "../../component/admin/AdminSideBar";
+import AdminMainContainer from "../../component/admin/AdminMainContainer";
+
+const Admin = () => {
+    return (
+        <Box p="0 20px" h="100vh"  w="100vw" d="flex" justifyContent="center" alignItems="center">
+            <AdminSideBar/>
+            <AdminMainContainer/>
+        </Box>
+    );
+};
+
+export default Admin;

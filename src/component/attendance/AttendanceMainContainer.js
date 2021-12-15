7e22d9c662e9e2ca98047c3757b691a74020327f
kindyluv <onyeukwuamara@gmail.com>
2021-12-15 03:07:47 +0100

    ..

---
+++ src/component/attendance/AttendanceMainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react";
+
+const AttendanceMainContainer = () => {
+    return (
+        <Box w="80%" h="95%" bg="#E3FBF2" rounded="20px" ml="20px">
+
+        </Box>
+    );
+};
+
+export default AttendanceMainContainer;

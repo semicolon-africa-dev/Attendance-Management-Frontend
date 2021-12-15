61a0791afafe3133dad6ec9e310e676eebceb2e8
kindyluv <onyeukwuamara@gmail.com>
2021-12-15 03:07:43 +0100

    ..

---
+++ src/views/native/Attendance.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Box} from '@chakra-ui/react'
+import AttendanceSideBar from "../../component/attendance/AttendanceSideBar";
+import AttendanceMainContainer from "../../component/attendance/AttendanceMainContainer";
+
+const Attendance = () => {
+    return (
+        <Box p="0 20px" h="100vh"  w="100vw" d="flex" justifyContent="center" alignItems="center">
+            <AttendanceSideBar/>
+            <AttendanceMainContainer/>
+        </Box>
+    );
+};
+
+export default Attendance;

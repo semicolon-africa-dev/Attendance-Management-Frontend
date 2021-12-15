0891e997a3b8c5cdf86a007946358c565d94c2d2
kindyluv <onyeukwuamara@gmail.com>
2021-12-15 03:07:37 +0100

    ..

---
+++ src/component/admin/AdminSideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react";
+
+const AdminSideBar = () => {
+    return (
+        <Box bg="#E3FBF2" w="20%" h="95%" rounded="20px">
+
+        </Box>
+    );
+};
+
+export default AdminSideBar;

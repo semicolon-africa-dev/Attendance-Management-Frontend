ebf3075070e008b765d39f0b81f8d56960787835
kindyluv <onyeukwuamara@gmail.com>
2021-12-16 18:34:54 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Admin from "./views/admin/Admin";
-import Attendance from "./views/native/Attendance";
+import Attendance from "./views/attendance/Attendance";
 
 function App() {
   return (

ffe2c526a83a80ba3d64d50b4b7ea6865bce53c5
kindyluv <onyeukwuamara@gmail.com>
2021-12-25 10:28:02 +0100

    ..

--- src/component/admin/AdminMainContainer.js
+++ src/component/admin/AdminMainContainer.js
@@ -3,7 +3,7 @@ import {Box} from "@chakra-ui/react";
 
 const AdminMainContainer = () => {
     return (
-        <Box w="80%" h="95%" bg="#E3FBF2" rounded="20px" ml="20px" justify="center" align="center">
+        <Box fontStyle={"normal"} fontFamily={"Montserrat"} w="80%" h="95%" bg="#E3FBF2" rounded="20px" ml="20px" justify="center" align="center">
             Post
         </Box>
     );

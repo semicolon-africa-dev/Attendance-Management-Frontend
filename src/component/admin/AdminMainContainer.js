57005e179e392c320a5c944cb1292bd72b3f7316
Oludairo <eoludairo61@gmail.com>
2021-12-29 12:44:27 +0100

    added profile dropdown menu

--- src/component/admin/AdminMainContainer.js
+++ src/component/admin/AdminMainContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Box} from "@chakra-ui/react";
+import Header from './Header';
 
 const AdminMainContainer = () => {
     return (
         <Box w="80%" h="95%" bg="#E3FBF2" rounded="20px" ml="20px">
-
+            <Header/>
         </Box>
     );
 };

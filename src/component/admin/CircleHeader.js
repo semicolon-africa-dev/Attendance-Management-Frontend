b20e65adbb20bf1efc0518db90c30939cb6332e5
forverchild <dabreezy8@gmail.com>
2022-01-07 12:38:54 +0100

    added admin up bar component

---
+++ src/component/admin/CircleHeader.js
@@ -0,0 +1,19 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const CircleHeader = () => {
+  return (
+    
+        <Box height="150px" width="150px" backgroundColor="
+        #FF113A" borderRadius="50%" pt="20px">
+          <Box height="110px" width="110px" backgroundColor="#E3FBF2" borderRadius="50%" ml="20px">
+
+          </Box>
+      
+        </Box>
+    
+   
+  )
+}
+
+export default CircleHeader

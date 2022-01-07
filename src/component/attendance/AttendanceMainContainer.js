59f48ff31814695aa5d801906cb87fa9ab380879
Yetife <tifetreats21@gmail.com>
2022-01-07 21:10:18 +0100

    Native Daily Attendance

--- src/component/attendance/AttendanceMainContainer.js
+++ src/component/attendance/AttendanceMainContainer.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box} from "@chakra-ui/react";
+import Lowbar from "./Lowbar";
 
 const AttendanceMainContainer = () => {
+
     return (
         <Box w="80%" h="95%" bg="#E3FBF2" rounded="20px" ml="20px">
-
+            <Lowbar />
         </Box>
     );
 };

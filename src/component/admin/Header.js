b57b42773451e4f52bc5b872dcc190d95cc6c128
Oludairo <eoludairo61@gmail.com>
2022-01-15 12:59:48 +0100

    updated admin profile icon

--- src/component/admin/Header.js
+++ src/component/admin/Header.js
@@ -11,19 +11,19 @@ import edit_profile from '../../assets/edit_profile-icon.svg'
 
 
 const Header = () => {
-    const [state, setstate] = useState("hidden")
+    const [dropdownState, setDropdownState] = useState("hidden")
     let email='patiencetommy@gmail.com'
     return (
         <Box>
             <Box display="flex" float="right" mt="22px" mr="25px" pr="30px" pt="0.5px">
                
-               <ProfileIcon state={state} setstate = {setstate}/>
+               <ProfileIcon dropdownState={dropdownState} setDropdownState = {setDropdownState}/>
                 <Box paddingLeft="15px" paddingTop="8px">
-                    <img src= {dropdown} onClick={()=>{state==="hidden"?setstate("visible"):setstate("hidden")}} alt="dropdown-icon"></img>
+                    <img src= {dropdown} onClick={()=>{dropdownState==="hidden"?setDropdownState("visible"):setDropdownState("hidden")}} alt="dropdown-icon"></img>
                 </Box>
             </Box>
             
-          <List listStyleType="none" w="250px" h="300px" bg="#ffffff" float="right" mt="50px" rounded="10px" textAlign="center" visibility={state} mb="10px" paddingTop="30px">
+          <List listStyleType="none" w="250px" h="300px" bg="#ffffff" float="right" mt="50px" rounded="10px" textAlign="center" visibility={dropdownState} mb="10px" paddingTop="30px">
                 <Box textAlign = "center" fontSize="small" display="flex" ml="25px">
                     <ProfileIcon/>
                     <ListItem fontWeight="bold" ml="15px">{ email } </ListItem>
--- src/component/admin/ProfileIcon.js
+++ src/component/admin/ProfileIcon.js
@@ -1,16 +1,16 @@
 import {React} from 'react'
-import { Box } from '@chakra-ui/layout'
+import { Box, Center } from '@chakra-ui/layout'
 import {Text} from '@chakra-ui/react'
 
 const ProfileIcon = (props) => {
     let firstLetter = "P";
-    const state = props.state;
-    const setState = props.setstate;
+    const dropdownState = props.dropdownState;
+    const setDropdownState = props.setDropdownState;
     return (
-         <Box h="22px" w="22px" bg="#ff113a" rounded="30px" textAlign="center" textColor="#ffffff" onClick={()=>{state==="hidden"?setState("visible"):setState("hidden")}}>
-                <Box paddingTop="2px" fontWeight="extrabold">
-                    <Text fontSize="small">{firstLetter}</Text>
-                </Box>
+         <Box h="40px" w="40px" bg="#ff113a" rounded="30px" textAlign="center" textColor="#ffffff" onClick={()=>{dropdownState==="hidden"?setDropdownState("visible"):setDropdownState("hidden")}}>
+             <Center mt="-8px" fontWeight="extrabold">
+                <Text fontSize="large" mb="20px">{firstLetter}</Text>
+             </Center>
          </Box>
             
     )
